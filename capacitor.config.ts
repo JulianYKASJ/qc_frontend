6a384b5a58475655c0dffbab1d2711c1348288af
Andrés Giraldo <agiraldodev@gmail.com>
2024-05-04 16:46:27 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'qc_frontend',
+  webDir: 'dist'
+};
+
+export default config;
