0910996f06a0792264cb2261e3fcd8ada1753179
Julian Naranjo <128192350+JulianYKASJ@users.noreply.github.com>
2024-05-25 09:23:54 -0500

    haciendo pantalla aulas

---
+++ src/views/AulasPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <ion-content>
+    <ion-header class="aulas">
+        <ion-toolbar>
+          <ion-title>
+            MIS AULAS
+          </ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+      <ion-header class="menu">
+        <ion-toolbar>
+          <ion-title>
+            MENU INFERIOR
+          </ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+
+  </ion-content>
+</template>
+
+<script setup lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import ExploreContainer from '@/components/ExploreContainer.vue';
+</script>
+
+<style>
+.aulas{
+  text-align: center;
+}
+
+</style>
